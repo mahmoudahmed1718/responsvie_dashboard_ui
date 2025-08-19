10603d386e985c1319fa78650ac3ea7b4be75d05
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-19 16:27:55 +0300

    add images and create asset file in utils to get image easy to use it for readme file

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesDesktoView
+  /// assets/images/deskto_view.jpg
+  static const String assetsImagesDesktoView = "assets/images/deskto_view.jpg";
+
+  /// Assets for assetsImagesDesktopViewWithDrawer
+  /// assets/images/desktop_view-with_drawer.jpg
+  static const String assetsImagesDesktopViewWithDrawer = "assets/images/desktop_view-with_drawer.jpg";
+
+  /// Assets for assetsImagesMobilView
+  /// assets/images/mobil_view.jpg
+  static const String assetsImagesMobilView = "assets/images/mobil_view.jpg";
+
+  /// Assets for assetsImagesMobileViewWithdrawer
+  /// assets/images/mobile_view_withdrawer.jpg
+  static const String assetsImagesMobileViewWithdrawer = "assets/images/mobile_view_withdrawer.jpg";
+
+  /// Assets for assetsImagesTabletView
+  /// assets/images/tablet_view.jpg
+  static const String assetsImagesTabletView = "assets/images/tablet_view.jpg";
+}
+
