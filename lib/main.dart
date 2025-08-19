98044270dbb6509442d68191f787d4afb91cd103
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-19 14:15:10 +0300

    create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsvie_dashboard_ui/page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +9,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false);
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
