01c88f97ae33353634506c10bb5ca7cb2b113791
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-19 14:26:44 +0300

    create and finsh responsvie layout file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsvie_dashboard_ui/page/home_page.dart';
+import 'package:responsvie_dashboard_ui/responsive/responsive_layout.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +9,13 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ResponsiveLayout(
+        mobileScreenLayout: Scaffold(body: Center(child: Text("Mobile"))),
+        tabletScreenLayout: Scaffold(body: Center(child: Text("Tablet"))),
+        desktopScreenLayout: Scaffold(body: Center(child: Text("Desktop"))),
+      ),
+    );
   }
 }
---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({
+    super.key,
+    required this.mobileScreenLayout,
+    required this.tabletScreenLayout,
+    required this.desktopScreenLayout,
+  });
+  final Widget mobileScreenLayout;
+  final Widget tabletScreenLayout;
+  final Widget desktopScreenLayout;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return mobileScreenLayout;
+        } else if (constraints.maxWidth < 1100) {
+          return tabletScreenLayout;
+        } else {
+          return desktopScreenLayout;
+        }
+      },
+    );
+  }
+}
